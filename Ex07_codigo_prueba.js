ff94ac8d736d867fda157a8522b20ebeb7ccb434
Santo <santicardonal777@gmail.com>
2021-02-02 12:38:47 -0500

    ejercicios 07 y 09

---
+++ Ex07_codigo_prueba.js
@@ -0,0 +1,13 @@
+console.log('Javascriot es bueno');
+
+function saludar(nombre){
+    console.log(`Hola ${nombre}`)
+}
+
+saludar('Santo');
+
+setTimeout(() => {
+    console.log('Este mensaje se mostrara pasdo 5 segundos'), 5000;
+});
+
+console.log('Fin')
---
+++ Ex09_declaracion_variables.js
@@ -0,0 +1,16 @@
+let numero; 
+numero = 7;
+
+console.log(numero);
+
+console.log(typeof(numero))
+
+numero = '7';
+
+console.log(numero);
+
+console.log(typeof(numero))
+
+var x, y;
+var a = 1, b = 'Santo';
+
